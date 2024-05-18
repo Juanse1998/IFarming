132754832ff505c68d7031fd995389f16b24c424
juanse <juansesosa98@gmail.com>
2024-05-18 20:18:14 -0300

    add react native elements

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Input, Icon } from 'react-native-elements';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Input 
+        containerStyle={{width: '50%'}}
+        placeholder="aca"
+      ></Input>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,6 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: '50%',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
