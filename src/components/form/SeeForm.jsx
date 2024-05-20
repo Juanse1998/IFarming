4fc783a02f6a4db791252dff35344098f2d1cb45
juanse <juansesosa98@gmail.com>
2024-05-20 15:48:05 -0300

    Se agrego logica para ver un formulario

--- src/components/form/SeeForm.jsx
+++ src/components/form/SeeForm.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import { View,Text } from "react-native-web";
+import { View, Text } from "react-native-web";
 import { Input } from "react-native-elements";
-
 
 const SeeForm = ({ route }) => {
   const { form } = route.params;
-  console.log('formulario', form);
+
   return (
     <View style={{ flex: 1, display: 'flex', flexDirection: 'column', marginVertical: 10 }}>
     <Text>{form.name}</Text>
     {form.fields.map((element, index) => (
       <View key={index} style={{ marginVertical: 10 }}>
-        <Text>{element.fieldName}</Text>
         <Input
+          label={element.fieldName}
           placeholder={element.placeholder}
           keyboardType={element.inputType}
           style={{ borderWidth: 1, padding: 5 }}
