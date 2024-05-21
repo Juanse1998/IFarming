15d11e503a0e0707befdb4fc816401ccfe303ae8
juanse <juansesosa98@gmail.com>
2024-05-21 18:34:17 -0300

    Se agrego estilo en CreateForm

--- src/components/form/CreateForm.jsx
+++ src/components/form/CreateForm.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { addField, addForm } from "../../redux/actions/action";
-import { View, Button, TextInput, Text } from "react-native-web";
+import { View, Button, TextInput, Text, StyleSheet, Platform } from "react-native";
 import { useNavigation } from '@react-navigation/native'; 
 import Modal from 'react-native-modal';
 
 const CreateForm = ({ addForm, addField }) => {
-  const [formName, setFormName] = useState(null);
+  const [formName, setFormName] = useState('');
   const [isModalVisible, setModalVisible] = useState(false);
   const [error, setError] = useState(null);
 
@@ -34,25 +34,89 @@ const CreateForm = ({ addForm, addField }) => {
   };
 
   return (
-    <View style={{ padding: 20 }}>
-      <TextInput
-        value={formName}
-        onChangeText={setFormName}
-        placeholder="Nombre del Formulario"
-        style={{ marginBottom: 10, borderWidth: 1, padding: 5 }}
-      />
-       {error && <Text style={{ color: 'red' }}>{error}</Text>}
-      <Button title="Agregar Formulario" onPress={handleAddForm} />
-      <Button title="Ver Formularios" onPress={handleSeeForms} />
-      <Modal isVisible={isModalVisible} onBackdropPress={showModal}>
-        <View style={{ backgroundColor: 'white', padding: 20 }}>
-          <Text>¡Formulario creado con exito!</Text>
+  <View style={styles.container}>
+    <View style={styles.containerCard}>
+      <Text style={styles.title}>CREA TU FORMULARIO</Text>
+        <Text style={styles.label}>Ingrese el nombre del formulario:</Text>
+        <TextInput
+          value={formName}
+          onChangeText={setFormName}
+          placeholder="Ej: formulario 1"
+          style={styles.input}
+        />
+        {error && <Text style={styles.error}>{error}</Text>}
+        <View style={styles.buttonContainer}>
+          <Button title="Agregar Formulario" onPress={handleAddForm} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button title="Ver Formularios" onPress={handleSeeForms} />
+        </View>
+      <Modal style={{alignItems: 'center'}} isVisible={isModalVisible} onBackdropPress={showModal}>
+        <View style={styles.modalContent}>
+          <Text style={styles.modalText}>¡Formulario creado con éxito!</Text>
           <Button title="Cerrar" onPress={showModal} />
         </View>
       </Modal>
     </View>
-  )
+  </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Platform.OS === 'web' ? '#fff' : '#1E40AF', 
+    width: '100%',
+    padding: 20,
+  },
+  containerCard: {
+    alignItems: 'center',
+    backgroundColor: Platform.OS === 'web' ? '#fff' : '#1E40AF', 
+    width: Platform.OS === 'web' ? '300px' : '60%',
+    padding: 20,
+    border: '1px solid black',
+    boxShadow: '5px 5px'
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 50
+  },
+  label: {
+    fontSize: 15,
+    marginBottom: 10,
+    color: Platform.OS === 'web' ? '#000' : '#fff', 
+  },
+  input: {
+    backgroundColor: '#fff',
+    padding: 10,
+    width: Platform.OS === 'web' ? '200px' : '60%', 
+    borderRadius: 5,
+    marginBottom: 10,
+    border: '1px solid black'
+  },
+  error: {
+    color: 'red',
+    marginBottom: 10,
+    width: Platform.OS === 'web' ? '200px' : '60%',
+  },
+  buttonContainer: {
+    width: Platform.OS === 'web' ? '200px' : '60%',
+    marginBottom: 10,
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+    width: Platform.OS === 'web' ? '280px' : '60%', 
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+});
 
 const mapStateToProps = state => ({
   forms: state.form.forms
